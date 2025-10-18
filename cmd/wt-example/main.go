f24529b5eae252b2def1f72cae57bb36c8bff732
iyifr <adekileiyimide@gmail.com>
2025-10-18 19:31:13 +0100

    feat: rough range scan implementation

--- cmd/wt-example/main.go
+++ cmd/wt-example/main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	wt "glowstickdb/pkgs/wiredtiger"
-)
-
 func main() {
-	if err := wt.RunExample(); err != nil {
-		fmt.Println("example error:", err)
-	}
+	// if err := wt.RunExample(); err != nil {
+	// 	fmt.Println("example error:", err)
+	// }
+
+	RunDetailedRangeScanPerformanceTest()
 }
